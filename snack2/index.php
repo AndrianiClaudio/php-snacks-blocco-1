d945bb2e9f3bba96d5d7ee8826c5f5f27e14672d
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-01-31 15:15:37 +0100

    Ricezione dati tramite $_GET di name,mail ed age

--- snack2/index.php
+++ snack2/index.php
@@ -11,4 +11,15 @@ verificare
 Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
 ========
 */
+// otteniamo name,mail,age tramite GET
+$name = $_GET['name'];
+$mail = $_GET['mail'];
+$age = $_GET['age'];
+// STAMPA TEST RICEZIONE DATI CORRETTA
+// echo 'name<br>';
+// var_dump($name);
+// echo '<br>mail<br>';
+// var_dump($mail);
+// echo '<br>age<br>';
+// var_dump($age);
 ?>
