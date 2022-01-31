b0449ae6625655caa888d30e5ed0d612db61dece
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-01-31 18:22:25 +0100

    Creato Snack5/index.php

---
+++ snack5/index.php
@@ -0,0 +1,21 @@
+<?php
+/*
+CONSEGNA
+========
+Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
+========
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php snack 1 - 5</title>
+</head>
+<body>
+    <h1>Snack 5</h1>
+</body>
+</html>
