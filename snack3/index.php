c0e67cee06218c5fe0e667b70e93790359ae952f
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-01-31 15:56:38 +0100

    Creata cartella Snack3

---
+++ snack3/index.php
@@ -0,0 +1,36 @@
+<?php
+/*
+CONSEGNA
+========
+Creare un array di array. 
+Ogni array figlio avrà
+    chiave: data (DD-MM-YYYY es 01-01-2007)
+    valore: array di post associati alla data. 
+Stampare ogni data con i relativi post.
+----------------
+ARRAY DI ESEMPIO
+----------------
+$posts = [
+    '10/01/2019' => [
+        'title' => 'Post'
+        'author' => 'Claudio Andriani'
+        'text' => 'Text'
+    ],...
+]
+----------------
+========
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Snack 1 - 3</title>
+</head>
+<body>
+    <h1>Snack 3</h1>
+</body>
+</html>
