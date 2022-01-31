9bd8b7b80ceab5a9ad917e144bacec4bc0e71c64
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-01-31 14:45:31 +0100

    Creazione repo. Snack2/index.php

---
+++ snack2/index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+CONSEGNA
+========
+Passare come parametri GET
+    name, mail e age
+verificare
+    name sia più lungo di 3 caratteri
+    mail contenga un punto e una chiocciola
+    age sia un numero. 
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+========
+*/
+?>
